571116297698b0f93a41048c5e878932aea1eca0
prasadcm <BG1MACMINI011@PrasadCM.local>
2013-08-30 19:15:30 +0530

    Commit Test

--- GitTest/ViewController.m
+++ GitTest/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.title = @"sfgsg";
+    self.title = @"Git test";
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
